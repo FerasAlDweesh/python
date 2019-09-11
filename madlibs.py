5375abf8836e0cb612fcacb6c19fb35cda4256bc
Feras AlDweesh <ferasaldweesh@gmail.com>
2019-09-11 22:16:44 +0300

    editid in sublime text

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+#Mad libs where libs get mad.
+#Start below:
+
+time = input("Enter a number:  ")
+items = input("Enter a noun: ")
+name = input("Enter a name: ")
+scream = input("Enter any sentence: ")
+actions = input("Enter a verb: ")
+
+print("It was", time, "o'clock when I heard a knock at the door.")
+print("I opened the door and there was a", items, "with a note saying From", name.title())
+print("Just as I closed the door I heard a scream", scream.upper())
+print("I froze in place and all I could do was", actions)
