e0554c71f2f9d129dc1085f89df0000ca1a8eba3
Feras AlDweesh <ferasaldweesh@gmail.com>
2019-09-23 20:56:31 +0300

    corrections made:

--- age_calculator.py
+++ age_calculator.py
@@ -1,18 +1,24 @@
-from datetime import date
-todays_date = date.today()
-print("Today's date is", todays_date)
+from datetime import datetime
+
+def check_birthdate(year, month, day):
+	todays_date = datetime.now()
+	birthdate = datetime(birth_year, birth_month, birth_day)
+	if birthdate > todays_date:
+		return False
+	else:
+		return True
+
+def calculate_age(year, month, day):
+	todays_date = datetime.now()
+	birthdate = datetime(birth_year, birth_month, birth_day)
+	age = todays_date - birthdate
+	print("you are %d years old" % (age.days / 365))
 
 birth_year = int(input("Enter the year you were born:  "))
 birth_month = int(input("Enter the month you were born:  "))
 birth_day = int(input("Enter the day you were born:  "))
 
-
-def check_birthdate(year, month, day):
-	if birth_year > todays_date:
-		return False
-
-def calculate_age(year, month, day):
-	age = date.year() - birth_year
-
-
-print("you are", calculate_age(birth_year, birth_month, birth_day), "years old")
+if check_birthdate(birth_year, birth_month, birth_day):
+	calculate_age(birth_year, birth_month, birth_day)
+else:
+	print("Invalid date")
